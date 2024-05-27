ff746443a98d9e189b56cc4076a427276f3e7d28
jcorderx <asid@deb-ho.search.charter.net>
2024-05-27 14:11:55 -0400

    Added more functionality for the database. Database now gets updated with new game findings and only sends a message with new games that are found

--- Reddit Free Games Parser.py
+++ Reddit Free Games Parser.py
@@ -9,7 +9,9 @@ todo:
 
 
 """
-
+import re
+import mysql.connector
+from mysql.connector import Error
 from config import CONFIGURATIONS
 import requests
 from bs4 import BeautifulSoup
@@ -17,13 +19,13 @@ import smtplib
 import sys
 import logging
 from email.message import EmailMessage
-import mariadb
 
 
 CARRIERS = CONFIGURATIONS['carriers']
 EMAIL = CONFIGURATIONS['credentials']['email']
 PASSWORD = CONFIGURATIONS['credentials']['password']
 PHONENUMBER = CONFIGURATIONS['credentials']['phone']
+DATABASE = CONFIGURATIONS['database']
 
 def send_message(phone_number, carrier, message):
     msg = EmailMessage()
@@ -39,27 +41,113 @@ def send_message(phone_number, carrier, message):
  
     server.sendmail(auth[0], recipient, message)
 
+
+def extract_numeric_value(url):
+    # Define a regex pattern to match numeric values in the URL
+    pattern = re.compile(r'\d+')
+    
+    # Search for the pattern in the URL
+    match = re.search(pattern, url)
+    
+    # If a match is found, return the numeric value as a string
+    if match:
+        return match.group()
+    else:
+        return None  # Return None if no numeric value is found
+
+
+#pulls url data from html document and creates a set of urls to eliminate duplicates
 def parseData(soup,stores):
     htmlResultList = []
     for a in soup.find_all('a', href=True) :
         try:
+            link = (a['href'].split('/')[2])
             for store in stores:
-                link = (a['href'].split('/')[2])
                 if ((link == store) and ('domain' not in a['href']) ):
                     htmlResultList.append(a['href'])
-                
-
         except:
             continue
-    return htmlResultList
+    return [*set(htmlResultList)]
 
-def insertIntoDatabase(htmlResultList, cur, ):
-    cur.execute(
-    "insert"
-    )
-    
-    print("hello")
 
+def epicGogQuery(platform, cursor, name, url, connection, messageArray):
+    # Check if the combination of Name and URL already exists
+    query = f"SELECT * FROM {platform} WHERE Name = %s AND URL = %s"
+    cursor.execute(query, (name, url))
+    result = cursor.fetchone()
+
+    if result:
+        print(f"The game '{name}' already exists in the '{platform}' database.")
+    else:
+        # Insert the new entry
+        insert_query = f"INSERT INTO {platform} (Name, URL) VALUES (%s, %s)"
+        cursor.execute(insert_query, (name, url))
+        connection.commit()
+        print(f"New game '{name}' inserted successfully into '{platform}'.")
+        return messageArray.append("""\n""" + name + """\n""" + url + """,\n""")
+
+
+def steamQuery(platform, cursor, name, url, connection, numeric_value, messageArray):
+    # Check if the combination of Name and URL already exists
+    query = f"SELECT * FROM {platform} WHERE GameID = %s AND Name = %s AND URL = %s"
+    cursor.execute(query, (numeric_value, name, url))
+    result = cursor.fetchone()
+
+    if result:
+        print(f"The game '{name}' already exists in the '{platform}' database.")
+    else:
+        # Insert the new entry
+        insert_query = f"INSERT INTO {platform} (GameID, Name, URL) VALUES (%s, %s, %s)"
+        cursor.execute(insert_query, (numeric_value, name, url))
+        connection.commit()
+        print(f"New game '{name}' inserted successfully into '{platform}'.")
+        return messageArray.append("""\n""" + name + """\n""" + url + """,\n""")
+
+
+def check_and_insert(gameList, messageArray):
+    try:
+        connection = mysql.connector.connect(
+            host=DATABASE['host'],
+            database=DATABASE['database'],
+            user=DATABASE['user'],
+            password=DATABASE['password']
+        )
+        cursor = connection.cursor()
+
+        for platform, games in gameList.items():
+            for game_id, game_info in games.items():
+                name = game_id
+                url = game_info['url']
+
+                if platform == 'steam':
+                    numeric_value = extract_numeric_value(url)
+                    steamQuery(platform, cursor, name, url, connection, numeric_value, messageArray)
+                else:
+                    epicGogQuery(platform, cursor, name, url, connection, messageArray)
+
+
+
+    except Error as e:
+        print(f"Error: {e}")
+
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+
+# Function to extract platform names
+def extract_platform(url, pattern):
+    match = pattern.search(url)
+    if match:
+        platform = match.group(1)
+        return platform_mapping.get(platform, platform)
+    return None
+
+# Function to add a game to the dictionary under a given key
+def gameDict(my_dict, platform, game_id, url, name):
+    if platform not in my_dict:
+        my_dict[platform] = {}
+    my_dict[platform][game_id] = {'url': url, 'name': name}
 
 
 if __name__ == "__main__":
@@ -68,8 +156,20 @@ if __name__ == "__main__":
     #Log File config
     logging.basicConfig(filename='parser.log', encoding='utf-8', level=logging.DEBUG)
 
-    gameList = []
-    tmp = []
+    # Define a mapping for known cases
+    platform_mapping = {
+        'steampowered': 'steam',
+        'epicgames': 'epic',
+        'gog': 'gog'
+    }
+
+    gameList = {}
+    newGameList = []
+    messageArray = []
+
+
+    # Define a pattern to match the desired part of the URLs
+    pattern = re.compile(r'(\b\w+)(?=\.com)')
 
     # Request the url
     url = "https://old.reddit.com/r/FreeGameFindings/"
@@ -84,21 +184,27 @@ if __name__ == "__main__":
     stores = ["store.steampowered.com","www.microsoft.com","store.epicgames.com","www.gog.com"]
 
     htmlResultList = parseData(soup, stores)
-    htmlResultList = [*set(htmlResultList)]
 
-    for item in htmlResultList:
-        tempString = item.split("/")
+    #goes through and just pulls out the name of the game from the url/htmlResultList
+    for urlItem in htmlResultList:
+        tempString = urlItem.split("/")
         gameName = tempString.pop()
         if(gameName == ""):
             gameName = tempString.pop()
-        gameName = gameName.replace("-", " ")
-        gameName = gameName.replace("_", " ")
-        #gameList.append(gameName)
-        tmp.append("""\n""" + gameName + """\n""" + item + """,\n""")
 
+        gameName = gameName.replace("-", " ").replace("_", " ")
+        
+        # Extract platform names from the URLs
+        storeName = extract_platform(urlItem, pattern)
+        
+        #stores information for database in gameList for use with messages later
+        gameDict(gameList, storeName, gameName, urlItem, gameName)
+        
   
-
-    message = """ """.join(tmp)
+    check_and_insert(gameList, messageArray)
+  
+    #should only contain new games that have not been entered into the database so we don't get duplicates
+    message = """ """.join(messageArray)
 
 
     logging.info(message)
